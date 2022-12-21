3be95b637791736a8e79fc83f47219a2e906e06c
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-12-21 11:53:23 +0400

    commit DZ1

---
+++ DZ1.sem9/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая
+//  выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($" {N} -> {PrintNumRec(N)}");
+
+
+
+string PrintNumRec(int N)
+{
+    if (N == 1) return "1";
+    return Convert.ToString(N) + ", " + PrintNumRec(N - 1) ;
+}
