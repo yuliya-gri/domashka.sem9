0d5da32152fa67d9c0cfbf72cd91c20fe826a87f
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-12-21 12:01:45 +0400

    commit DZ2

---
+++ DZ2.sem9/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.WriteLine("Напишите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ($"Сумма элементов в промежутке от {M} до {N} равна {sumRec(M, N)}");
+
+int sumRec( int M, int N)
+{
+    if(M == N) return N ;
+    return N + sumRec(M, N - 1 );
+}
